3b07ee58a556594ab9a2775ffc63cbf4c52c6a04
Anmol Verma <anmolverma102002@gmail.com>
2023-04-02 18:32:11 +0530

    feat: :compass: added ticket event

--- nats-test/src/events/ticket-created-event.ts
+++ nats-test/src/events/ticket-created-event.ts
@@ -2,7 +2,7 @@ import { Subjects } from './subjects';
 
 /**
  * Using this method we can take care of type checking
- * In future if we have multiple event's we can just list them out here.
+ * In future if we have multiple events we can just list them out here.
  */
 export interface TicketCreatedEvent {
 	subject: Subjects.TicketCreated;
