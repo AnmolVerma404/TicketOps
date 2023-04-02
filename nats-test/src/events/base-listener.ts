26502ea17dc3f4ff45546b35158de33beb1b6584
Anmol Verma <anmolverma102002@gmail.com>
2023-04-02 19:01:46 +0530

    feat: :sparkles: added generic class in TS for type check

--- nats-test/src/events/base-listener.ts
+++ nats-test/src/events/base-listener.ts
@@ -1,12 +1,18 @@
 import { Message, Stan } from 'node-nats-streaming';
+import { Subjects } from './subjects';
+
+interface Event {
+	subject: Subjects;
+	data: any;
+}
 
 /**
  * abstract class will be defined in sub class of Listener
  */
-export abstract class Listener {
-	abstract subject: string;
+export abstract class Listener<T extends Event> {
+	abstract subject: T['subject'];
 	abstract queueGroupName: string;
-	abstract onMessage(data: any, msg: Message): void;
+	abstract onMessage(data: T['data'], msg: Message): void;
 	private client: Stan;
 	protected ackWait = 5 * 1000;
 
--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,10 +1,12 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from './base-listener';
-export class TicketCreatedListener extends Listener {
-	subject = 'ticket:created';
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+	subject: Subjects.TicketCreated = Subjects.TicketCreated;
 	queueGroupName = 'payments-service';
 
-	onMessage(data: any, msg: Message) {
+	onMessage(data: TicketCreatedEvent['data'], msg: Message) {
 		console.log('Event data!', data);
 
 		msg.ack();
