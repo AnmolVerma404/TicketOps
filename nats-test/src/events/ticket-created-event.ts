bac41484818af4afcc4b01596549d5132ac3446a
Anmol Verma <anmolverma102002@gmail.com>
2023-04-02 18:30:50 +0530

    feat: :soft_ice_cream: added ticket event

--- nats-test/src/events/ticket-created-event.ts
+++ nats-test/src/events/ticket-created-event.ts
@@ -1,5 +1,9 @@
 import { Subjects } from './subjects';
 
+/**
+ * Using this method we can take care of type checking
+ * In future if we have multiple event's we can just list them out here.
+ */
 export interface TicketCreatedEvent {
 	subject: Subjects.TicketCreated;
 	data: {
