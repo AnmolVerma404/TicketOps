d7836eadb6ddbef990be0a04bc9fc80276524e1d
Anmol Verma <anmolverma102002@gmail.com>
2023-05-19 19:21:10 +0530

    chore: :tada: orders init

---
+++ orders/src/app.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { errorHandler, NotFoundError, currentUser } from '@avtickets404/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+/**
+ * secure: process.env.NODE_ENV !== 'test'
+ * When jest run the test it's auto set secure: process.env.NODE_ENV = 'test'
+ */
+app.use(
+	cookieSession({
+		signed: false,
+		secure: process.env.NODE_ENV !== 'test',
+	})
+);
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', async () => {
+	throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
