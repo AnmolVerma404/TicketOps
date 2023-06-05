6b4d179c15597e8547f98312ac33740ff55612f2
Anmol Verma <anmolverma102002@gmail.com>
2023-06-05 16:09:15 +0530

    feat: :sparkles: added index test

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,9 +1,14 @@
+import { requireAuth } from '@avtickets404/common';
 import express, { Request, Response } from 'express';
+import { Order } from '../modals/order';
 
 const router = express.Router();
 
-router.get('api/orders', async (req, res) => {
-	res.send({});
+router.get('/api/orders', requireAuth, async (req: Request, res: Response) => {
+	const orders = await Order.find({
+		userId: req.currentUser!.id,
+	}).populate('ticket');
+	res.send(orders);
 });
 
 export { router as indexOrderRouter };
