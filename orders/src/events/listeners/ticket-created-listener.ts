c3c6321dbefb9ec23a018269daa44e494d24e4b8
Anmol Verma <anmolverma102002@gmail.com>
2023-06-08 01:54:54 +0530

    feat: :tv: added basic ticket updated listener

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -8,8 +8,9 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
 	queueGroupName = queueGroupName;
 
 	async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
-		const { title, price } = data;
+		const { id, title, price } = data;
 		const ticket = Ticket.build({
+			id,
 			title,
 			price,
 		});
--- orders/src/modals/ticket.ts
+++ orders/src/modals/ticket.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import { Order, OrderStatus } from './order';
 
 interface TicketAttrs {
+	id: string;
 	title: string;
 	price: number;
 }
@@ -43,7 +44,11 @@ const ticketSchema = new mongoose.Schema(
  */
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-	return new Ticket(attrs);
+	return new Ticket({
+		_id: attrs.id,
+		title: attrs.title,
+		price: attrs.price,
+	});
 };
 
 /**
