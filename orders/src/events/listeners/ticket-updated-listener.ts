1e5a63eafccd619f95f93125f42f59233a9fa140
Anmol Verma <anmolverma102002@gmail.com>
2023-06-10 13:09:55 +0530

    feat: :tada: implemented optimistic concurrency control in for tickets

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -8,7 +8,17 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
 	queueGroupName = queueGroupName;
 
 	async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
-		const ticket = await Ticket.findById(data.id);
+		/**
+		 * The next DB query is really important regarding optimistic concurrency control
+		 * It will se and try to find if the ticket id's version correct or not
+		 * That is the version that is given by Ticket Microservice should be currVersion - 1
+		 * Eg -> In ticket MS updation looks like -> 10$ V0 -> 15$ V1 -> 20$ V2
+		 * Now if we have version 2 TicketUpdatedEvent that means V1 should be present. Otherwise it will cause concurrency issue.
+		 */
+		const ticket = await Ticket.findOne({
+			_id: data.id,
+			version: data.version - 1,
+		});
 
 		if (!ticket) {
 			throw new Error('Ticket not found');
