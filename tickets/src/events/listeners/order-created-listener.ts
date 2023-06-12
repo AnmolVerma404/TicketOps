5cffc56eb00ec0f7c3179fa28daef639c21153ab
Anmol Verma <anmolverma102002@gmail.com>
2023-06-12 22:55:48 +0530

    feat: :sparkles: order created list -> ticketUpd Publisher & updated access specifier for nats server

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -7,6 +7,7 @@ import {
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
 import { Ticket } from '../../models/ticket';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 	subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -25,6 +26,15 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
 		ticket.set({ orderId: data.id });
 		await ticket.save();
+
+		await new TicketUpdatedPublisher(this.client).publish({
+			id: ticket.id,
+			price: ticket.price,
+			title: ticket.title,
+			userId: ticket.userId,
+			orderId: ticket.orderId,
+			version: ticket.version,
+		});
 
 		msg.ack();
 	}
