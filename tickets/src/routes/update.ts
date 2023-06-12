c950918a1a3eb637da34077af84025e94cd8ea55
Anmol Verma <anmolverma102002@gmail.com>
2023-06-13 00:09:10 +0530

    feat: :tada: added test and listeners for order in ticket MS

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -5,6 +5,7 @@ import {
 	NotAuthorizedError,
 	NotFoundError,
 	requireAuth,
+	BadRequestError,
 } from '@avtickets404/common';
 import { Ticket } from '../models/ticket';
 import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
@@ -27,6 +28,10 @@ router.put(
 
 		if (!ticket) {
 			throw new NotFoundError();
+		}
+
+		if (ticket.orderId) {
+			throw new BadRequestError('Cannot edit a reserved ticket');
 		}
 
 		if (ticket.userId !== req.currentUser!.id) {
