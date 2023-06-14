6fcdd3e4ef9898bc9d6fe431ca74b9e20f1ba99f
Anmol Verma <anmolverma102002@gmail.com>
2023-06-14 16:01:29 +0530

    feat: :sparkles: added expliration complete listener

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -7,17 +7,31 @@ import {
 import { queueGroupName } from './queue-group-name';
 import { Message } from 'node-nats-streaming';
 import { Order } from '../../models/order';
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
+import { natsWrapper } from '../../nats-wrapper';
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
 	subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
 	queueGroupName = queueGroupName;
 	async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
-		const order = await Order.findById(data.orderId);
+		const order = await Order.findById(data.orderId).populate('ticket');
 
 		if (!order) {
 			throw new Error('Order not found');
 		}
 
-		order.set({ status: OrderStatus.Cancelled, ticket: null });
+		order.set({ status: OrderStatus.Cancelled });
+
+		await order.save();
+
+		await new OrderCancelledPublisher(this.client).publish({
+			id: order.id,
+			version: order.version,
+			ticket: {
+				id: order.ticket.id,
+			},
+		});
+
+		msg.ack();
 	}
 }
