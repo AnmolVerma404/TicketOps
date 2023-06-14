7c6c5803c9a855a7a921456ff910a9dbaea948fb
Anmol Verma <anmolverma102002@gmail.com>
2023-06-14 14:45:18 +0530

    feat: :hourglass: updated expiration service listener and update common modules

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -8,9 +8,20 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 	queueGroupName = queueGroupName;
 
 	async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-		await expirationQueue.add({
-			orderId: data.id,
-		});
+		const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+		console.log('Waiting this many milliseconds to process the job: ', delay);
+
+		/**
+		 * This is the main step where we pass the data to the bull queue
+		 */
+		await expirationQueue.add(
+			{
+				orderId: data.id,
+			},
+			{
+				delay,
+			}
+		);
 
 		msg.ack();
 	}
