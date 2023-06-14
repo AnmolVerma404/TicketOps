fa1e37ad38b48bd39cadeb8aaeabb806fa9efc7c
Anmol Verma <anmolverma102002@gmail.com>
2023-06-14 17:06:23 +0530

    feat: :sparkles: added listener to index

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -3,6 +3,7 @@ import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
 import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
 import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
+import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener';
 
 const start = async () => {
 	if (!process.env.JWT_KEY) {
@@ -43,6 +44,7 @@ const start = async () => {
 
 		new TicketCreatedListener(natsWrapper.client).listen();
 		new TicketUpdatedListener(natsWrapper.client).listen();
+		new ExpirationCompleteListener(natsWrapper.client).listen();
 
 		/**
 		 * This will allow us to connect to a local database of mongoDB.
