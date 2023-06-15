e3c3a1e3b11825c38c28565bfd048a028a44bb38
Anmol Verma <anmolverma102002@gmail.com>
2023-06-15 16:57:24 +0530

    feat: :sparkles: finilizing the listeners

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
+import { OrderCancelledListener } from './events/listeners/order-cancelled.listener';
 
 const start = async () => {
 	if (!process.env.JWT_KEY) {
@@ -36,6 +38,9 @@ const start = async () => {
 
 		process.on('SiGINT', () => natsWrapper.client!.close()); // ctrl + s || rs the server
 		process.on('SIGTERM', () => natsWrapper.client!.close()); // ctrl + c terminate the terminal
+
+		new OrderCreatedListener(natsWrapper.client).listen();
+		new OrderCancelledListener(natsWrapper.client).listen();
 
 		/**
 		 * This will allow us to connect to a local database of mongoDB.
