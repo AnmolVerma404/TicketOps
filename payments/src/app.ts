074ebbdcefeab7f4067fed908de3c459b301b688
Anmol Verma <anmolverma102002@gmail.com>
2023-06-15 14:38:07 +0530

    chore: :tada: payment microservice init setup

---
+++ payments/src/app.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { errorHandler, NotFoundError, currentUser } from '@avtickets404/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+/**
+ * secure: process.env.NODE_ENV !== 'test'
+ * When jest run the test it's auto set secure: process.env.NODE_ENV = 'test'
+ */
+app.use(
+	cookieSession({
+		signed: false,
+		secure: process.env.NODE_ENV !== 'test',
+	})
+);
+app.use(currentUser);
+
+app.all('*', async () => {
+	throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
