cf0ca301234967986ed7a593f95dd7314724b075
Anmol Verma <anmolverma102002@gmail.com>
2023-06-16 21:53:03 +0530

    err: :x: stripe charges create is not longer supported in india

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 const router = express.Router();
 
@@ -18,6 +19,10 @@ router.post(
 	[body('token').not().isEmpty(), body('orderId').not().isEmpty()],
 	validateRequest,
 	async (req: Request, res: Response) => {
+		/**
+		 * @var token will be including infomations like credit card details.
+		 * @var orderId defines the order that is making the payment, using which we can search for the user and other information.
+		 */
 		const { token, orderId } = req.body;
 
 		const order = await Order.findById(orderId);
@@ -31,6 +36,15 @@ router.post(
 		if (order.status === OrderStatus.Cancelled) {
 			throw new BadRequestError('Cannot pay for an cancelled order');
 		}
+		/**
+		 * RBI have changed some rules, so his method is not working
+		 * Try to find a new method
+		 */
+		await stripe.charges.create({
+			currency: 'usd',
+			amount: order.price * 100,
+			source: token,
+		});
 
 		res.send({ success: true });
 	}
