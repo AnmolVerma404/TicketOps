113c0cd7a813b83491471d1b714ce11d68f10c0d
Anmol Verma <anmolverma102002@gmail.com>
2023-06-19 16:01:13 +0530

    feat: :sparkles: rendered list of ticket

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,29 @@
 
-const LandingPage = ({ currentUser }) => {
-	return currentUser ? (
-		<h1>You are signed in</h1>
-	) : (
-		<h1>You are not signed in</h1>
+const LandingPage = ({ currentUser,tickets }) => {
+	const ticketList = tickets.map(ticket =>{
+		return (
+			<tr key={ticket.id}>
+				<td>{ticket.title}</td>
+				<td>{ticket.price}</td>
+			</tr>
+		)
+	})
+
+	return (
+		<div>
+			<h1>Tickets</h1>
+			<table className="table">
+				<thead>
+					<tr>
+						<th>Title</th>
+						<th>Price</th>
+					</tr>
+				</thead>
+				<tbody>
+					{ticketList}
+				</tbody>
+			</table>
+		</div>
 	);
 };
 
@@ -19,8 +39,10 @@ const LandingPage = ({ currentUser }) => {
  * We can't use custom or any hooks in getInitialProps, as it's not a next function
  * Therefore we need to make a request from axios
  */
-LandingPage.getInitialProps = async context => {
-	return {};
+LandingPage.getInitialProps = async (context, client, currentUser)=> {
+	const {data} = await client.get('/api/tickets');
+
+	return {tickets:data};
 };
 
 export default LandingPage;
