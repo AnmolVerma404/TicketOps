d4fe1adea25778f49652111817eabf1d40630224
Anmol Verma <anmolverma102002@gmail.com>
2023-06-19 16:40:23 +0530

    feat: :sparkles: added link in ticket's for redirections

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 const LandingPage = ({ currentUser,tickets }) => {
 	const ticketList = tickets.map(ticket =>{
@@ -5,6 +6,11 @@ const LandingPage = ({ currentUser,tickets }) => {
 			<tr key={ticket.id}>
 				<td>{ticket.title}</td>
 				<td>{ticket.price}</td>
+				<td>
+					<Link legacyBehavior href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+						<a>View</a>
+					</Link>
+				</td>
 			</tr>
 		)
 	})
@@ -17,6 +23,7 @@ const LandingPage = ({ currentUser,tickets }) => {
 					<tr>
 						<th>Title</th>
 						<th>Price</th>
+						<th>Link</th>
 					</tr>
 				</thead>
 				<tbody>
