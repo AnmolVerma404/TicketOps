3bb6592e322be4f6d15592b9730ddf5b2c7ba173
Anmol Verma <anmolverma102002@gmail.com>
2023-06-20 16:34:58 +0530

    feat: :sparkles: added payment->order redirection

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import StripeCheckout from 'react-stripe-checkout';
 import useRequest from "../../hooks/use-request";
+import Router from "next/router";
 
 
 const OrderShow = ({order,currentUser}) =>{
@@ -11,7 +12,7 @@ const OrderShow = ({order,currentUser}) =>{
         body:{
             orderId:order.id,
         },
-        onSuccess:(payment)=>console.log(payment)
+        onSuccess:()=>Router.push('/orders')
     });
 
     useEffect(()=>{
