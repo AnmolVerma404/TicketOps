74e471d8730853a4063dec930c7f9d495a1ba007
Anmol Verma <anmolverma102002@gmail.com>
2023-06-20 16:17:25 +0530

    feat: :sparkles: connected payment gateway, backend RBI policy remains

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,8 +1,18 @@
 import { useEffect, useState } from "react";
 import StripeCheckout from 'react-stripe-checkout';
+import useRequest from "../../hooks/use-request";
+
 
 const OrderShow = ({order,currentUser}) =>{
     const [timeLeft, setTimeLeft] = useState(0);
+    const {doRequest,errors} = useRequest({
+        url:'/api/payments',
+        method:'post',
+        body:{
+            orderId:order.id,
+        },
+        onSuccess:(payment)=>console.log(payment)
+    });
 
     useEffect(()=>{
         const findTimeLeft = () =>{
@@ -22,11 +32,12 @@ const OrderShow = ({order,currentUser}) =>{
 
     return (<div>Time left to pay: {timeLeft} seconds
         <StripeCheckout
-            token={(token)=>console.log(token)}
+            token={({id})=>doRequest({token:id})}
             stripeKey="pk_test_51NJbIzSDBQLRzv161xKsDKrXPMm41OMI4uhPGELWQUhHjhHEEHKpIXsQxsrOjt6u2c8EnJsNiAXcSYkHbzaFeuk700EkPFHtSh"
             amount={order.ticket.price*100}
             email={currentUser.email}
         />
+        {errors}
     </div>);
 }
 
