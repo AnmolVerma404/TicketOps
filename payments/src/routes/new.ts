0081f2c690e6f28d4f3ba2a90ea85faf6c08e4b8
Anmol Verma <anmolverma102002@gmail.com>
2023-06-21 19:51:02 +0530

    fix: :ambulance: fixed payment RBI bug, via 3D transaction, left some concurrency issues

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -42,75 +42,46 @@ router.post(
 		/**
 		 * RBI have changed some rules, so his method is not working
 		 * Try to find a new method
-		 */
-		// await stripe.charges.create({
-		// 	currency: 'inr',
-		// 	amount: order.price * 100,
-		// 	source: token,
-		// });
-
-		/**
 		 * This stripe payment method was mentioned in docs
 		 * Link -> https://stripe.com/docs/checkout/quickstart?lang=node&client=react
 		 * tried many ways but they were not working, so skipping payment part for later
 		 */
-		// const paymentIntent = await stripe.paymentIntents.create({
-		// 	amount: order.price * 100,
-		// 	currency: 'inr',
-		// 	automatic_payment_methods: {
-		// 		enabled: true,
-		// 	},
-		// 	payment_method_options: {
-		// 		card: {
-		// 			cvc_token: 'toc_visa',
-		// 		},
-		// 	},
-		// 	// @ts-ignore
-		// 	payment_method_data: {
-		// 		billing_details: {
-		// 			address: {
-		// 				city: 'test',
-		// 				country: 'test',
-		// 				line1: 'test',
-		// 				line2: 'test',
-		// 				postal_code: 'test',
-		// 				state: 'test',
-		// 			},
-		// 			email: 'test@test.com',
-		// 			name: 'test',
-		// 			phone: '9876543210',
-		// 		},
-		// 	},
-		// });
+		console.log('order', order);
 
-		// const customer = await stripe.customers.create({
-		// 	email: 'test@test.com',
-		// 	source: token,
-		// 	name: 'AV',
-		// 	address: {
-		// 		line1: 'abc',
-		// 		postal_code: '123411',
-		// 		city: 'Bhopal',
-		// 		state: 'MP',
-		// 		country: 'India',
-		// 	},
-		// });
-		// const charge = await stripe.charges.create({
-		// 	amount: 100 * 100,
-		// 	description: 'abc',
-		// 	currency: 'INR',
-		// 	customer: customer.id,
-		// });
-		// stripe.paymentIntents.create({
-		// 	customer: customer.id,
-		// 	amount: 100 * 100,
-		// 	description: 'Rails Stripe transaction',
-		// 	currency: 'INR',
-		// });
+		const items = [
+			{
+				name: 'Temp',
+				price: order.price,
+				quantity: 1,
+			},
+		];
+
+		const session = await stripe.checkout.sessions.create({
+			payment_method_types: ['card'],
+			mode: 'payment',
+			line_items: items.map(
+				(item: { name: any; price: any; quantity: any }) => {
+					return {
+						price_data: {
+							currency: 'inr',
+							product_data: {
+								name: item.name,
+							},
+							unit_amount: item.price * 100,
+						},
+						quantity: item.quantity,
+					};
+				}
+			),
+			success_url: 'https://ticketing.dev/orders',
+			cancel_url: 'https://ticketing.dev/orders',
+		});
+
+		console.log('session=>', session);
 
 		const payment = Payment.build({
 			orderId,
-			stripeId: 'WAIT_TILL_FRONTEND_FOR_STRIPE_PAYMENT_INTENTS',
+			stripeId: session.id,
 		});
 		await payment.save();
 
@@ -121,7 +92,7 @@ router.post(
 		});
 
 		// res.send({ clientSecret: paymentIntent.client_secret, success: true });
-		res.status(201).send({ id: payment.id });
+		res.status(201).send({ id: payment.id, url: session.url });
 	}
 );
 
